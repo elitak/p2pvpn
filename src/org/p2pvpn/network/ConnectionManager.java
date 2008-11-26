34e1873f2e8d54dd0219acc6875274aea66b253c
wginolas <wginolas@9f7ff10d-5e0a-443e-82e5-f308f1b4d886>
2008-11-26 13:45:39 +0000

    tagged 0.1
    git-svn-id: https://p2pvpn.svn.sf.net/svnroot/p2pvpn/p2pvpn/trunk@18 9f7ff10d-5e0a-443e-82e5-f308f1b4d886

--- src/org/p2pvpn/network/ConnectionManager.java
+++ src/org/p2pvpn/network/ConnectionManager.java
@@ -21,10 +21,8 @@ package org.p2pvpn.network;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
 import java.net.Inet4Address;
 import java.net.InetAddress;
-import java.net.MalformedURLException;
 import java.net.NetworkInterface;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -35,8 +33,6 @@ import java.util.Enumeration;
 import java.util.StringTokenizer;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 
 public class ConnectionManager implements Runnable {
